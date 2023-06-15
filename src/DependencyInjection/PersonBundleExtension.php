94a44160bd85b600ee5c138cf6d5a3df2ce6af32
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-06-15 15:25:48 +0300

    19

--- src/DependencyInjection/PersonBundleExtension.php
+++ src/DependencyInjection/PersonBundleExtension.php
@@ -15,6 +15,7 @@ class PersonBundleExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        dump(dirname(__DIR__).'/Resources/config');
         $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__).'/Resources/config'));
         $loader->load('services.yaml');
     }
